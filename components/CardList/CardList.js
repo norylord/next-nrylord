2f2aca3b6e47960f6498c6a833d075521ccfa9a1
nrylord <61661058+nrylord@users.noreply.github.com>
2022-08-04 15:49:03 +0300

    grid items

---
+++ components/CardList/CardList.js
@@ -0,0 +1,19 @@
+import styles from './CardList.module.sass'
+
+const CardList = () => {
+    return (
+        <div className={styles.card__list}>
+            <div className={styles.card__item}></div>
+            <div className={styles.card__item}></div>
+            <div className={styles.card__item}></div>
+            <div className={styles.card__item}></div>
+            <div className={styles.card__item}></div>
+            <div className={styles.card__item}></div>
+            <div className={styles.card__item}></div>
+            <div className={styles.card__item}></div>
+            <div className={styles.card__item}></div>
+        </div>
+    );
+};
+
+export default CardList;
--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import styles from '../styles/Home.module.sass'
 import Input from "../components/UI/Input/Input";
 import Button from "../components/UI/Button/Button";
 import CardCreateForm from "../components/CardCreateForm/CardCreateForm";
+import CardList from "../components/CardList/CardList";
 
 export default function Home() {
     return (
@@ -11,9 +12,8 @@ export default function Home() {
             <h1 className={styles.title}>Добавление товара</h1>
             <div className={styles.main__container}>
                 <CardCreateForm/>
-                <div className={styles.cards__container}></div>
+                <CardList/>
             </div>
-
         </div>
     )
 }
