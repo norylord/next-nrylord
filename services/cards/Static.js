0d4f4b6e48e4a3b4e765aa9592567e7aac5ae8aa
nrylord <61661058+nrylord@users.noreply.github.com>
2022-08-10 14:18:36 +0300

    123

--- services/cards/Static.js
+++ services/cards/Static.js
@@ -6,7 +6,7 @@ export const staticData = Object.freeze({
             description: "Довольно-таки интересное описание товара в несколько строк. Довольно-таки интересное описание товара в несколько строк",
             price: 10000,
             currency: "RUB",
-            image: 'https://sun9-east.userapi.com/sun9-25/s/v1/ig2/OCUovPBwyl-gvm9HdrvjJfWXS8FfrUbADtTE3KghqTzPH2ygX0GGm9DgijXEi4IAVVFOGbjH2rcTuJHFfwmw11SM.jpg?size=640x640&quality=95&type=album'
+            image: 'https://sun9-east.userapi.com/sun9-35/s/v1/ig2/6rUtDp8RylhZ0CBaCJzUmvlW5sZHI9UG83-LC5IAeWJe0VKg45nEJPnJd8uylQEME5G_Xms9k_-diFfn_5dRkRdu.jpg?size=2560x1707&quality=95&type=album'
         },
         {
             id: 2,
@@ -14,7 +14,7 @@ export const staticData = Object.freeze({
             description: "Довольно-таки интересное описание товара в несколько строк. Довольно-таки интересное описание товара в несколько строк",
             price: 20000,
             currency: "RUB",
-            image: 'https://sun9-east.userapi.com/sun9-25/s/v1/ig2/OCUovPBwyl-gvm9HdrvjJfWXS8FfrUbADtTE3KghqTzPH2ygX0GGm9DgijXEi4IAVVFOGbjH2rcTuJHFfwmw11SM.jpg?size=640x640&quality=95&type=album'
+            image: 'https://sun9-east.userapi.com/sun9-35/s/v1/ig2/6rUtDp8RylhZ0CBaCJzUmvlW5sZHI9UG83-LC5IAeWJe0VKg45nEJPnJd8uylQEME5G_Xms9k_-diFfn_5dRkRdu.jpg?size=2560x1707&quality=95&type=album'
         },
     ]
 })
